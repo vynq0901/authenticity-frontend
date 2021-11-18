dc9ba92bfdff2656643285084fb6c3058ec12c17
unknown <vy.nq0901@gmail.com>
2021-11-18 22:43:09 +0800

    fix cors chat

--- src/features/Staff/pages/SupporterPage.jsx
+++ src/features/Staff/pages/SupporterPage.jsx
@@ -36,7 +36,7 @@ const SupporterPage = () => {
     // }, [])
     useEffect(() => {
         if (!socket) {
-            const st = io.connect('http://localhost:5000')
+            const st = io.connect('https://authenticity-bend.herokuapp.com')
             setSocket(st)
         }
         if (socket) {
